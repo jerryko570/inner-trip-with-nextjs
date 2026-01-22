f988ee9f8def23739835a8d61f5805f2fd97172f
jerryko570 <jerry.narae@gmail.com>
2026-01-22 12:16:58 +0900

    chore: svg를 react 컴포넌트로 사용하기 위한 처리 및 환경설정

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,3 +1,5 @@
+// 이 프로젝트의 커밋 메시지는 Conventional Commits 규칙 그대로 따른다.
+// type은 소문자이고 feat, fix, docs, style, refactor, test, chore 시용
 module.exports = {
   extends: ['@commitlint/config-conventional'],
 };
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+// SVG 파일을 React 컴포넌트처럼 import 해서 사용 가능하고,
+// Turbopack에게 SVGR 로더를 적용하라고 알려주는 실제 변환 빌드 설정
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
