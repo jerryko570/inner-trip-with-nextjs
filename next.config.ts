dc05fa01b1b634c21041ffce720f9861471a0e0a
jerryko570 <jerry.narae@gmail.com>
2026-01-22 15:15:25 +0900

    chore: vite-plugin-svgr 설정 삭제

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
-// SVG 파일을 React 컴포넌트처럼 import 해서 사용 가능하고,
-// Turbopack에게 SVGR 로더를 적용하라고 알려주는 실제 변환 빌드 설정
-
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
