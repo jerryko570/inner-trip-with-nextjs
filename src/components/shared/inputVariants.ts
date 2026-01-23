63a683b6780e289e3e914e377ba47578e1371b8a
jerryko570 <jerry.narae@gmail.com>
2026-01-23 23:52:59 +0900

    chore: 전체 파일에 Prettier 포맷팅 적용

---
+++ src/components/shared/inputVariants.ts
@@ -0,0 +1,16 @@
+import { cva } from 'class-variance-authority';
+
+export const InputVariants = cva(
+  'w-full h-[54px] border border-gray-30 pl-4 rounded-lg focus:outline-none',
+  {
+    variants: {
+      status: {
+        default: 'focus:border-primary-50',
+        error: 'border-red-20',
+      },
+    },
+    defaultVariants: {
+      status: 'default',
+    },
+  },
+);
