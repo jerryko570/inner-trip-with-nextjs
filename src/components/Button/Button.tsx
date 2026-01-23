3a4321d287d21ebe0d9b2c0b6a84a48fe9a13a3c
jerryko570 <jerry.narae@gmail.com>
2026-01-23 12:36:26 +0900

    refactor: 코드 리뷰 피드백 반영

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,25 @@
 import { VariantProps } from "class-variance-authority";
+
 import { ButtonVariants } from "./styles";
 import { cn } from "@/lib/cn";
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof ButtonVariants>;
 
-export default function Button({ variant, className, ...props }: ButtonProps) {
+export default function Button({
+  variant,
+  size,
+  fullWidth,
+  className,
+  children,
+  ...props
+}: ButtonProps) {
   return (
-    <button {...props} className={cn(ButtonVariants({ variant }), className)} />
+    <button
+      {...props}
+      className={cn(ButtonVariants({ variant, size, fullWidth }), className)}
+    >
+      {children}
+    </button>
   );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -8,6 +8,14 @@ export const ButtonVariants = cva(
         primary: "bg-primary-40 hover:bg-primary-50",
         disabled: "bg-gray-30",
       },
+      size: {
+        md: "h-[54px] px-4 text-lg",
+        sm: "h-[40px] px-3 text-sm",
+      },
+      fullWidth: {
+        true: "w-full",
+        false: "w-auto",
+      },
     },
     defaultVariants: {
       variant: "primary",
