5973ae9197e3432367a12c87b20a3afff77c999c
jerryko570 <jerry.narae@gmail.com>
2026-01-23 19:19:24 +0900

    refactor: 코드 리뷰 피드백 반영

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 import Input from "@/components/Input/Input";
 import PasswordInput from "@/components/PasswordInput/PasswordInput";
@@ -13,9 +14,17 @@ export default function SignupPage() {
 
   return (
     <div className="flex flex-col items-center gap-20 pt-20">
-      <Image src="/logo.svg" alt="logo" width={400} height={100} />
+      <Link href="/">
+        <Image
+          src="/logo.svg"
+          alt="logo"
+          width={400}
+          height={100}
+          className="cursor-pointer"
+        />
+      </Link>
 
-      <div className="w-full max-w-[500px] space-y-4">
+      <form className="w-full max-w-[500px] space-y-4">
         <Input
           status="default"
           placeholder="아이디를 입력하세요"
@@ -32,16 +41,18 @@ export default function SignupPage() {
         />
 
         <div className="pt-4">
-          <Button variant="primary">회원가입</Button>
+          <Button type="submit" variant="primary">
+            회원가입
+          </Button>
         </div>
 
         <p className="flex items-center justify-center pt-4">
-          <span className="text-bg">회원이 아니신가요?</span>
-          <a href="/login" className="pl-3 text-blue-20 bold">
+          <span className="text-body1">회원이 아니신가요?</span>
+          <Link href="/login" className="pl-3 text-body1 bold text-blue-20">
             로그인하기
-          </a>
+          </Link>
         </p>
-      </div>
+      </form>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-"use client";
-
-import Pagination from "../src/components/Pagination";
-import { useState } from "react";
-
-export default function Page() {
-  const [page, setPage] = useState(1);
-
-  return (
-    <Pagination currentPage={page} totalPages={5} onPageChange={setPage} />
-  );
+export default function HomePage() {
+  return <div>홈입니다</div>;
 }
