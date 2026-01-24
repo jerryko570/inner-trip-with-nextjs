af14f9ca2213b40677388240fc1c7a3105785a05
jerryko570 <jerry.narae@gmail.com>
2026-01-24 19:53:18 +0900

    feat: 로그인 관련 커스텀 훅 추가

---
+++ app/(auth)/login/useLogin.ts
@@ -0,0 +1,39 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { AuthValidation } from '@/lib/validation/authValidation';
+
+interface LoginFormData {
+  email: string;
+  password: string;
+}
+
+export default function useLogin() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormData>({
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (data: LoginFormData) => {
+    try {
+      console.log('회원가입 데이터:', data);
+
+      // TODO: 나중에 API 호출
+    } catch (error) {
+      console.error('회원가입 실패', error);
+    }
+  };
+
+  return {
+    register,
+    handleSubmit: handleSubmit(onSubmit),
+    setValue,
+    errors,
+    isSubmitting,
+    validation: AuthValidation,
+  };
+}
