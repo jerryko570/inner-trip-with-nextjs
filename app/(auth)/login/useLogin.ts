349e58a52e896334a2b1b945f7741e09b4ad3d88
jerryko570 <jerry.narae@gmail.com>
2026-01-24 19:58:39 +0900

    refactor: 코드 리뷰 피드백 반영

--- app/(auth)/login/useLogin.ts
+++ app/(auth)/login/useLogin.ts
@@ -20,11 +20,11 @@ export default function useLogin() {
 
   const onSubmit = async (data: LoginFormData) => {
     try {
-      console.log('회원가입 데이터:', data);
+      console.log('로그인 데이터:', data);
 
       // TODO: 나중에 API 호출
     } catch (error) {
-      console.error('회원가입 실패', error);
+      console.error('로그인 실패', error);
     }
   };
 
