83eec631be33aad0c0d164e4e9619e66ddacad58
jerryko570 <jerry.narae@gmail.com>
2026-03-13 09:51:26 +0900

    feat: header 컴포넌트 UI 시스템 구축

---
+++ src/components/Header/Header.stories.ts
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Header from './Header';
+
+const meta: Meta<typeof Header> = {
+  title: 'UI/Header',
+  component: Header,
+  argTypes: {
+    variant: { control: 'radio', options: ['default'] },
+    sticky: { control: 'boolean' },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    sticky: false,
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Transparent: Story = {
+  args: {
+    variant: 'default',
+    sticky: false,
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Sticky: Story = {
+  args: {
+    variant: 'default',
+    sticky: true,
+  },
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
